a89df8b962bafe61cbdf768f434a7456e9c97174
alius <alius@LAPTOP-NNR66SG1>
2021-03-04 20:03:47 +0100

    done

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 "use strict"
+// Global variabler av Github Api
 const apiUrl = 'https://api.github.com/users/';
 const main = document.querySelector('main');
 const search = document.querySelector('#search');
 const form = document.querySelector('form');
+
 // Creating Elements.
 const img = document.createElement('img');
 const h3 = document.createElement('h3');
 const h2 = document.createElement('h4');
 const p = document.createElement('p');
+
 // Get elementbyID 
 const get_meal_btn = document.getElementById('get_meal');
 const meal_container = document.getElementById('meal');
-//Movie Api
+
+//Movie Api+imagepath+searchapi
 const movieapiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const maincontent = document.getElementById("maincontent");
 const movieform = document.getElementById("movieform");
 const moviesearch = document.getElementById("moviesearch");
+
+
 
 
 
@@ -28,36 +32,40 @@ try {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     main.innerHTML = '';
-
-    const searchTerm = search.value;
-    if (searchTerm) {
+    let searchTerm;
+    if (search != null) {
+      searchTerm = search.value;
       searchProfile(apiUrl + searchTerm);
-      search.value = "";
     } else {
-      p.innerHTML = `Error:${"Please give the valid id"}`
+      return null;
     }
+    /*const searchTerm = search.value;
+    if(searchTerm == null){
+      alert("invalid value");
+    }
+    else{
+      searchProfile(apiUrl + searchTerm);
+    }
+     */
   });
-} catch (err) {
-  p.innerHTML = err.message;
-
+} catch (error) {
+  console.log(error);
 }
 
-// Fteching Data from the Api.
+// Fteching Data from the Github Api.
 function searchProfile(url) {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-
 
       // Appending those Elements to the main Element.
       main.appendChild(img);
       main.appendChild(h3);
       main.appendChild(h2);
       main.appendChild(p);
-      //main.appendChild(p1);
 
-      img.src = `${data.avatar_url}`
       // Putting the Api data on the elements.
+      img.src = `${data.avatar_url}`
       h3.innerHTML = ` Login : <a href="${data.html_url}" target="_blank">${data.login}</a>`
       h2.innerHTML = ` Name : ${data.name}`
       p.innerHTML = `Bio : ${data.bio}`
@@ -66,20 +74,23 @@ function searchProfile(url) {
 }
 
 
-
+// Random meal Api
+//ClickButtonevent to get the random food 
 try {
-  get_meal_btn.addEventListener('click', () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(res => res.json())
-      .then(res => {
-        createMeal(res.meals[0]);
+  get_meal_btn.addEventListener('click', async () => {
+    const serverresponse = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    const result = await serverresponse.json();
+    //.then(res => result.json())
+    //.then(res => {
+    createMeal(result.meals[0]);
 
-      });
   });
+
 } catch {
-  p.innerHTML = `Error:${"Please give the valid id"}`
+  p.innerHTML = `Error:${"something went wrong"}`
 }
 
+//creaTeMeal Function
 const createMeal = (meal) => {
   const ingredients = [];
   // Get all ingredients from the object. Up to 20
@@ -92,6 +103,7 @@ const createMeal = (meal) => {
     }
   }
 
+  // Uaing DOM to add and to show api data 
   const newInnerHTML = `
        <div class="row">
          <div class="columns five">
@@ -101,15 +113,15 @@ const createMeal = (meal) => {
            ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
            ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
            <h5>Ingredients:</h5>
-           
-             ${ingredients.map(ingredient => `${ingredient}`).join('')}
-           
-         </div>
+           ${ingredients.map(ingredient => `${ingredient}`).join('')}
+           </div>
+          
          <div class="columns seven">
            <h4>${meal.strMeal}</h4>
            <p>${meal.strInstructions}</p>
          </div>
        </div>
+
        ${meal.strYoutube ? `
        <div class="row">
          <h5>Video Recipe</h5>
@@ -120,42 +132,53 @@ const createMeal = (meal) => {
          </div>
        </div>` : ''}
      `;
-
   meal_container.innerHTML = newInnerHTML;
 }
-// Movie Api
-try{
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  main.innerHTML = '';
 
-  const searchTerm = moviesearch.value;
+// Form submit event to search the required movie
+try {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    let searchmovie;
+    if (moviesearch != null) {
+      searchmovie = moviesearch.value;
+      showMovies(SEARCHAPI + searchmovie);
+    } else {
+      return null;
+    }
+    //const searchmovie = moviesearch.value;
 
-  if (searchTerm) {
-    showMovies(SEARCHAPI + searchTerm);
-    search.value = "";
-  }
-});
-}catch{
+    /*if (searchmovie == 0) {
+      alert("invalid value")
+    }
+    else {
+      showMovies(SEARCHAPI + searchmovie);
+      
+    }
+    */
+  });
+} catch (error) {
   console.log(error);
 }
+//ShowMovies Function
 showMovies(movieapiUrl);
-
 function showMovies(url) {
   fetch(url).then(res => res.json())
     .then(function (data) {
-      console.log(data.results);
+
+
       data.results.forEach(element => {
         const el = document.createElement('div');
         const img = document.createElement('img');
-        
         const text = document.createElement('h2');
-
         text.innerHTML = `${element.title}`;
         img.src = IMGPATH + element.poster_path;
         el.appendChild(img);
         el.appendChild(text);
         maincontent.appendChild(el);
       });
+
     });
+
 }
